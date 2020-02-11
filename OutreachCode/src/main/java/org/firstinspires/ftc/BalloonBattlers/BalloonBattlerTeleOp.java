14136e7828273f1c043a9b066adcaa9a76d86cfd
Sei4or <sei4or@gmail.com>
2020-02-10 17:23:27 -0800

    more fixes

--- OutreachCode/src/main/java/org/firstinspires/ftc/BalloonBattlers/BalloonBattlerTeleOp.java
+++ OutreachCode/src/main/java/org/firstinspires/ftc/BalloonBattlers/BalloonBattlerTeleOp.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.BalloonBattlers;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.firstinspires.ftc.robotlib.robot.BalloonBattlerRobot;
 import org.firstinspires.ftc.robotlib.state.Button;
@@ -11,17 +10,17 @@ import org.firstinspires.ftc.robotlib.state.ServoState;
 @TeleOp(name="Balloon Battler TeleOp", group="Balloon")
 public class BalloonBattlerTeleOp extends OpMode
 {
-    /** Init **/
+    // Init
     private BalloonBattlerRobot robot;
     private Button attackButton;
 
-    /** Constants **/
+    // Constants
     private static final double ATTACK_POWER = 0.25;
 
     @Override
     public void init()
     {
-        /** Def **/
+        // Def
         robot = new BalloonBattlerRobot(this.hardwareMap);
         attackButton = new Button();
     }
@@ -29,21 +28,21 @@ public class BalloonBattlerTeleOp extends OpMode
     @Override
     public void loop()
     {
-        /** Update Drivetrain **/
+        // Update Drivetrain
         robot.drivetrain.setThrottle(gamepad1.right_trigger);
         robot.drivetrain.setBrake(gamepad1.left_trigger);
         robot.drivetrain.setRotation(gamepad1.right_stick_x/2);
 
-        /** Update Buttons **/
+        // Update Buttons
         attackButton.input(gamepad1.left_bumper);
 
-        /** Move arm **/
+        // Move arm
         if (attackButton.onPress() && robot.armPopper.getServoState() != ServoState.DOWN)
         { robot.armPopper.setTargetPosition(ServoState.DOWN, ATTACK_POWER); }
         else if (attackButton.onPress())
         { robot.armPopper.setTargetPosition(ServoState.UP, ATTACK_POWER); }
 
-        /** Telemetry **/
+        // Telemetry
         telemetry.addData("Throttle", robot.drivetrain.getThrottle());
         telemetry.addData("Rotation", robot.drivetrain.getRotation());
         telemetry.addData("Popper State", robot.armPopper.getServoState());
