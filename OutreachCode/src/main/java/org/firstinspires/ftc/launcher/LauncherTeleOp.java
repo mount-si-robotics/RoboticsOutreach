2a078fdbe9440792c3c02dd95e8aad296f93483f
Sei4or <sei4or@gmail.com>
2020-03-03 17:58:15 -0800

    Fixed hardware map Launcher still has bugs

--- OutreachCode/src/main/java/org/firstinspires/ftc/launcher/LauncherTeleOp.java
+++ OutreachCode/src/main/java/org/firstinspires/ftc/launcher/LauncherTeleOp.java
@@ -4,19 +4,38 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotlib.robot.LauncherRobot;
+import org.firstinspires.ftc.robotlib.state.Button;
 
 @TeleOp(name="Launcher TeleOp", group="teleop")
 public class LauncherTeleOp extends OpMode {
     private LauncherRobot robot;
 
+    // State
+    private boolean powerShooterMotors;
+
+    // Input
+    private Button yButton;
+
     @Override
     public void init() {
         robot = new LauncherRobot(hardwareMap);
+
+        yButton = new Button();
     }
 
     @Override
     public void loop() {
         robot.yawMotor.setPower(-gamepad1.right_stick_x);
         robot.pitchMotor.setPower(-gamepad1.left_stick_y);
+
+        if (powerShooterMotors) {
+            robot.setLauncherMotorPowers(gamepad1.right_trigger / 2 + 0.5);
+        } else {
+            robot.setLauncherMotorPowers(0.0);
+        }
+
+        if (yButton.onRelease()) powerShooterMotors = !powerShooterMotors;
+
+        yButton.input(gamepad1.y);
     }
 }
--- OutreachCode/src/main/java/org/firstinspires/ftc/robotlib/robot/LauncherRobot.java
+++ OutreachCode/src/main/java/org/firstinspires/ftc/robotlib/robot/LauncherRobot.java
@@ -2,18 +2,15 @@ package org.firstinspires.ftc.robotlib.robot;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotlib.drivetrain.ArcadeDrivetrain;
 import org.firstinspires.ftc.robotlib.motor.LimitedMotor;
 
 public class LauncherRobot {
+    public LimitedMotor rightLaunch;
+    public LimitedMotor leftLaunch;
     public LimitedMotor yawMotor;
     public LimitedMotor pitchMotor;
-    private DcMotor launcherMotor; // honestly don't remember this one
-
-    private DigitalChannel digitalTouchSensor;
 
     // Robot variables
     private final HardwareMap hwMap;
@@ -23,26 +20,29 @@ public class LauncherRobot {
         this.hwMap = hwMap;
 
         // Motor Init
+        rightLaunch = new LimitedMotor(hwMap.get(DcMotor.class, "right_launch"), 0, 100);
+        leftLaunch = new LimitedMotor(hwMap.get(DcMotor.class, "left_launch"), 0, 100);
         yawMotor = new LimitedMotor(hwMap.get(DcMotor.class, "yaw_motor"), 0, 100);
         pitchMotor = new LimitedMotor(hwMap.get(DcMotor.class, "pitch_motor"), 0, 100);
-        launcherMotor = hwMap.get(DcMotor.class, "launcher_motor");
 
+        rightLaunch.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftLaunch.setDirection(DcMotorSimple.Direction.FORWARD);
         yawMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         pitchMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        rightLaunch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftLaunch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         yawMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         pitchMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        rightLaunch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftLaunch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         yawMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         pitchMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        // Touch Sensor Init
-        digitalTouchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
-
-        digitalTouchSensor.setMode(DigitalChannel.Mode.INPUT);
     }
 
-    public boolean isTouchSensorPressed() {
-        return !digitalTouchSensor.getState();
+    public void setLauncherMotorPowers(double power) {
+        this.rightLaunch.setPower(power);
+        this.leftLaunch.setPower(power);
     }
 }
